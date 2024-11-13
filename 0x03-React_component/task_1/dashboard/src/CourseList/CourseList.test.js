b7f90b2d5d9c1484f5733394f53b96ea86d0974e
CObitimah <obitimahcephas@gmail.com>
2024-11-13 18:59:43 +0000

    task_1

---
+++ 0x03-React_component/task_1/dashboard/src/CourseList/CourseList.test.js
@@ -0,0 +1,12 @@
+
+// CourseList/CourseListRow.test.js
+import React from 'react';
+import { render } from '@testing-library/react';
+import CourseList from './CourseList';
+
+describe('CourseList does not crash', () => {
+    it('renders CourseList', () => {
+        render(<CourseList />)
+    });
+
+});
---
+++ 0x03-React_component/task_1/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+
+function NotificationItem({ type, html, value }) {
+    return (
+        <li
+            data-notification-type={type}
+            dangerouslySetInnerHTML={html}
+        >
+            {value}
+        </li>
+    )
+}
+
+export default NotificationItem
